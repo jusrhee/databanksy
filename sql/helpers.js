be2df13903053980491c5b365f70fe4356eea6a6
Alexander Belanger <abelanger@MiMIX2S-Lops.localdomain>
2019-11-11 14:12:21 -0500

    id as a string

--- sql/helpers.js
+++ sql/helpers.js
@@ -6,20 +6,19 @@ CREATE DATABASE databanksy;
 USE databanksy;
 
 CREATE TABLE artists(
-  ID int(20) UNIQUE NOT NULL,
+  ID VARCHAR(255) UNIQUE NOT NULL,
   name VARCHAR(255) NOT NULL,
   bio VARCHAR(2000),
   nationality VARCHAR(255),
   beginDate VARCHAR(255),
   endDate VARCHAR(255),
   dataSource VARCHAR(255),
-  artsy_ID VARCHAR(255),
   PRIMARY KEY(ID)
 );
 
 CREATE TABLE artworks(
-  artwork_ID int(20) UNIQUE NOT NULL,
-  creator_ID int(20) NOT NULL,
+  artwork_ID VARCHAR(255) UNIQUE NOT NULL,
+  creator_ID VARCHAR(255) NOT NULL,
   title VARCHAR(255) NOT NULL,
   date VARCHAR(255),
   cataloged VARCHAR(255),
@@ -51,21 +50,20 @@ sql.connect();
 
 // NEED TO ADD SERVER LINK/INITIALIZATION TO DEFINE var sql
 module.exports.add_verified_artist = async (obj) => {
-    let id = obj.ConstituentID;
+    let id = `"${obj.ConstituentID}"`;
     let name = `"${obj.DisplayName.replace(/"/g, '\\\"')}"`;
     let bio = obj.ArtistBio ? `"${obj.ArtistBio.replace(/"/g, '\\\"')}"` : 'NULL';
     let nationality = obj.Nationality ? `"${obj.Nationality}"` : 'NULL';;
     let beginDate = obj.BeginDate ? `"${obj.BeginDate}"` : 'NULL';
     let endDate = obj.EndDate ? `"${obj.EndDate}"` : 'NULL';
     let dataSource = obj.DataSource ? `"${obj.DataSource}"` : 'NULL';
-    let artsy_ID = obj.artsy_ID ? `"${obj.artsy_ID}"` : 'NULL';
 
     return new Promise((resolve, reject) => {
         sql.query(
           `INSERT INTO artists 
-          (ID, name, bio, nationality, beginDate, endDate, dataSource, artsy_ID) 
+          (ID, name, bio, nationality, beginDate, endDate, dataSource) 
           VALUES (${id}, ${name}, ${bio}, ${nationality}, ${beginDate}, ${endDate}, 
-          ${dataSource}, ${artsy_ID})`,
+          ${dataSource})`,
           function(err, res) {
             if (err) {
               reject(err);
@@ -80,7 +78,7 @@ module.exports.add_verified_artist = async (obj) => {
 module.exports.get_artsy_artists = async (obj) => {
     return new Promise((resolve, reject) => {
         sql.query(
-          `SELECT creator_ID FROM artists WHERE dataSource="Artsy"`,
+          `SELECT ID FROM artists WHERE dataSource="Artsy"`,
           function(err, res) {
             if (err) {
               reject(err);
@@ -112,23 +110,22 @@ module.exports.verify_artist_exists = (artist_id) => {
 }
 
 module.exports.add_verified_artwork = async (obj) => {
-    let creator_id = obj.ConstituentID;
-    let object_id = obj.ObjectID;
+    let creator_id = `"${obj.ConstituentID}"`;
+    let object_id = `"${obj.ObjectID}"`;
     let title = `"${obj.Title.replace(/"/g, '\\\"')}"`;
     let date = obj.Date ? `"${obj.Date}"` : 'NULL';
     let cataloged = obj.Cataloged ? `"${obj.Cataloged}"` : 'NULL';
     let classification = obj.Classification ? `"${obj.Classification}"` : 'NULL';
     let dataSource = obj.DataSource ? `"${obj.DataSource}"` : 'NULL';
-    let artsy_ID = obj.artsy_ID ? `"${obj.artsy_ID}"` : 'NULL';
     let image = obj.ThumbnailURL ? `"${obj.ThumbnailURL}"` : 'NULL';
 
     return new Promise((resolve, reject) => {
         sql.query(
           `INSERT INTO artworks 
           (artwork_ID, creator_ID, title, date, cataloged, classification,
-          dataSource, artsy_ID, image) 
+          dataSource, image) 
           VALUES (${object_id}, ${creator_id}, ${title}, ${date}, ${cataloged}, ${classification},
-          ${dataSource}, ${artsy_ID}, ${image})`,
+          ${dataSource}, ${image})`,
           function(err, res) {
             if (err) {
               reject(err);
