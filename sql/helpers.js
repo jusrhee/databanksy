c02163e0d38853a7ab44bd6ddac0f78bc955c2b0
Alexander Belanger <abelanger@Alexanders-MacBook-Pro-4.local>
2019-11-25 23:37:02 -0500

    added exhibition join

--- sql/helpers.js
+++ sql/helpers.js
@@ -30,6 +30,18 @@ CREATE TABLE artworks(
   FOREIGN KEY(creator_ID) REFERENCES artists(ID)
 );
 
+CREATE TABLE exhibitions(
+  exhibition_ID INT(10) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  beginDate date,
+  endDate date,
+  role VARCHAR(20),
+  artist_ID VARCHAR(255) NOT NULL,
+  artist_type VARCHAR(20),
+  PRIMARY KEY(exhibition_ID, artist_ID),
+  FOREIGN KEY(artist_ID) REFERENCES artists(ID)
+)
+
 ALTER TABLE databanksy.artists MODIFY COLUMN name VARCHAR(255)  
     CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
 
@@ -126,6 +138,32 @@ module.exports.add_verified_artwork = async (obj) => {
           dataSource, image) 
           VALUES (${object_id}, ${creator_id}, ${title}, ${date}, ${cataloged}, ${classification},
           ${dataSource}, ${image})`,
+          function(err, res) {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(true);
+            }
+          }
+        );
+    })
+}
+
+module.exports.add_verified_exhibition = async (obj) => {
+    let exhibition_ID = `"${obj.ExhibitionID}"`;
+    let title = `"${obj.ExhibitionTitle.replace(/"/g, '\\\"')}"`;
+    let beginDate = `"${obj.ExhibitionBeginDate}"`;
+    let endDate = `"${obj.ExhibitionEndDate}"`;
+    let role = obj.ExhibitionRole ? `"${obj.ExhibitionRole}"` : 'NULL';
+    let artist_ID = `"${obj.ConstituentID}"`;
+    let artist_type = obj.ConstituentType ? `"${obj.ConstituentType}"` : 'NULL';
+
+    return new Promise((resolve, reject) => {
+        sql.query(
+          `INSERT INTO artworks 
+          (exhibition_ID, title, beginDate, endDate, role, artist_ID, artist_type) 
+          VALUES (${exhibition_ID}, ${title}, ${beginDate}, ${endDate}, ${role}, ${artist_ID},
+          ${artist_type})`,
           function(err, res) {
             if (err) {
               reject(err);
